d334e3a84cd1dd5868762583f440522b2f301c92
Colin <colin.reitman87@gmail.com>
2019-05-13 18:50:34 -0400

    add post and user query types

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const app = express();
 const db = require('./config/keys').mongoURI;
 var bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const expressGraphQL = require('express-graphql');
+const User = require("./models/User");
+const schema = require("./schema/schema");
 
 app.get("/", (req, res) => res.send("Hello World"));
-app.use(bodyParser.json());
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema,
+    graphiql: true
+  })
+);
 
 mongoose
   .connect(db, { useNewUrlParser: true })
